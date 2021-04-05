d18f149ad2c1c69799056f1f933f725e39e6f113
Matthew Riegg <matthew.d.riegg@gmail.com>
2021-04-05 14:00:48 -0700

    Deliverables

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
+//3. Create a variable called `mod` set to an equation that will calculate the
+  // remainder of dividing variable `num3` by `num4`; the remainder should be `4`.
+//4. Create a variable called `max` that finds the highest number in a set; the
+  // value returned should be `20`.
 
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1*num2;
+
+
+
+const random = Math.floor(Math.random(1) * 10)
+
+console.log(random);
+
+const num3 = 31;
+const num4 = 9;
+const mod = num3%num4;
+
+const max = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+
+Math.max(max);
+
+const max = Math.max(1,2,20)
+
